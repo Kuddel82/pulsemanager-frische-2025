85ca7e286cc909112d6e2aefd1ad8572cabc6e7a
Kuddel182 <dkuddel@web.de>
2025-06-06 19:20:27 +0200

    Force Vercel cache bypass with environment variables

---
+++ force_cache_bypass.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "C:\Users\Anwender\Desktop\PulseManager_QUICKBACKUP_2025-06-05_22-27"
+git add -A
+git commit -m "Force Vercel cache bypass with environment variables"
+git push
+echo Push completed! 
