e0b0b7ebf5fe355c0b12a18e0e956e5692a7f2d3
Kuddel182 <dkuddel@web.de>
2025-06-19 18:04:57 +0200

    🔥 ALIAS API ERSTELLT: deutscher-steuerbericht.js als Alias für german-tax-report.js - Behebt 404 Fehler

---
+++ api/deutscher-steuerbericht.js
@@ -0,0 +1,12 @@
+/**
+ * 🇩🇪 DEUTSCHER-STEUERBERICHT API - ALIAS FÜR GERMAN-TAX-REPORT
+ * 
+ * Diese Datei ist ein Alias für german-tax-report.js
+ * Wird verwendet, wenn das Frontend /api/deutscher-steuerbericht aufruft
+ */
+
+// Importiere die Hauptfunktion aus german-tax-report.js
+import germanTaxReportHandler from './german-tax-report.js';
+
+// Exportiere die gleiche Funktion unter dem neuen Namen
+export default germanTaxReportHandler; 
